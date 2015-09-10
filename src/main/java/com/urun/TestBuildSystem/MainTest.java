41d3ba425d32f25fd39be422bc6f0d01171c1591
yanyamin <153673604@qq.com>
2015-09-10 15:35:57 +0800

    创建测试项目

---
+++ src/main/java/com/urun/TestBuildSystem/MainTest.java
@@ -0,0 +1,13 @@
+package com.urun.TestBuildSystem;
+
+public class MainTest {
+
+    public static void main(String[] args)
+    {
+        if(args[0]==null)
+        {
+            System.out.print("hello,world!");
+        }
+        System.out.println("hello,"+args[0]);
+    }
+}
